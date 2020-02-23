da9773965669a947b25ac1a889c2520f4ac922d3
Diego <dflores7237@gmail.com>
2020-02-23 11:07:29 -0800

    basic express server set up with two routes

--- server/app.js
+++ server/app.js
@@ -1,10 +1,22 @@
 // import files and packages up here
+const express = require('express');
+const morgan = require('morgan');
+const data = require('./data.json')
 
 
 // create your express server below
-var app;
+const app = express();
 
 // add your routes and middleware below
+app.use(morgan('dev'));
+
+app.get('/', function(req, res) {
+    res.status(200).send('New Express server up and running');
+})
+
+app.get('/data', function(req, res) {
+    res.json(data);
+})
 
 // finally export the express application
 module.exports = app;
